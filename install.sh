787b9d1fa587324d848900769beb018a8862a135
Tobias <loeh@zhaw.ch>
2016-11-23 12:01:53 +0100

    some bug fixes in the install.sh renamed a conf file

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# install required packages as root
+# store base path of execution
+BASEDIR=$(pwd)
+
 echo 'install required packages'
 sudo bash << EOF
 apt-get update
-apt-get install -y tar git curl nano wget dialog net-tools build-essential python2.7 python-pip numpy freenect gstreamer-0.10 gstreamer0.10-plugins-base gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-good cgroup-bin cgroup-lite libcgroup1
+apt-get install -y tar git curl nano wget dialog net-tools build-essential python2.7 python-pip freenect gstreamer-0.10 gstreamer0.10-plugins-base gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-good cgroup-bin cgroup-lite libcgroup1
+pip install numpy
 EOF
 
 # create catkin_workspace
@@ -15,43 +18,51 @@ cd /home/turtlebot/catkin_ws/src && catkin_init_workspace
 
 # copy ros packages to workspace
 echo 'copy ros packages to workspace'
-cp -r ./icclab_turtlebot /home/turtlebot/catkin_ws/src/icclab_turtlebot
 
-cp -r ./moveto /home/turtlebot/catkin_ws/src/moveto
+cd $BASEDIR
 
-cp -r ./rosnodeinfo /home/turtlebot/catkin_ws/src/rosnodeinfo
+cp -r icclab_turtlebot/ /home/turtlebot/catkin_ws/src/icclab_turtlebot
+
+cp -r moveto/ /home/turtlebot/catkin_ws/src/moveto
+
+cp -r rosnodeinfo/ /home/turtlebot/catkin_ws/src/rosnodeinfo
 
 git clone https://github.com/negre/rplidar_ros.git /home/turtlebot/catkin_ws/src/rplidar_ros
 
-git clone https://furbaz:<pass>@bitbucket.org/rapyutians/cloud_bridge.git /home/turtlebot/catkin_ws/src/cloud_bridge
+git clone https://furbaz:%40bitbucket2long@bitbucket.org/rapyutians/cloud_bridge.git /home/turtlebot/catkin_ws/src/cloud_bridge
 
 cd /home/turtlebot/catkin_ws && catkin_make
 
+cd $BASEDIR
 # copy launch scripts
 echo 'copy ros packages to workspace'
-cp ./launch_scripts/*.sh /home/turtlebot/
+cp launch_scripts/*.sh /home/turtlebot/
 
 # copy rplidar daemon script
 sudo bash << EOF
-cp ./udev/rplidar /etc/init.d
+cp udev/rplidar /etc/init.d
+chmod +x /etc/init.d/rplidar
 EOF
 
 # copy rplidar local rules
 sudo bash << EOF
-cp ./udev/10-local.rules /etc/udev/rules.d
+cp udev/10-local.rules /etc/udev/rules.d
 EOF
 
 # copy rplidar startup script
 sudo bash << EOF
 mkdir /etc/udev/scripts
-cp ./udev/rplidar.sh /etc/udev/scripts
+cp udev/rplidar.sh /etc/udev/scripts
+chmod +x /etc/udev/scripts/rplidar.sh
 EOF
 
 ## cgroup stuff
-cp ./cgroups/cgconfig.conf /etc
-cp ./cgroups/cgrules.conf /etc
+sudo bash << EOF
+cp cgroups/cgconfig.conf /etc
+cp cgroups/cgrules.conf /etc
 
-cp ./cgroups/cgrulesengd /etc/init
+cp cgroups/cgrulesengd.conf /etc/init
+EOF
 
 sudo bash << EOF
 service cgroup-lite start 
