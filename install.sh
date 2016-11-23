b2b9dd4fd63613208345cbdc71e394a5c48da3b6
loeh <loeh@zhaw.ch>
2016-11-23 17:06:33 +0100

    added cgroup lite basic config file added rplicadar startup conf added salt minion conf updated install script

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@
 BASEDIR=$(pwd)
 
 
-echo 'install required packages'
+echo 'installing required packages'
+echo '-----------------------'
 sudo bash << EOF
 apt-get update
-apt-get install -y tar git curl nano wget dialog net-tools build-essential python2.7 python-pip freenect gstreamer-0.10 gstreamer0.10-plugins-base gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-good cgroup-bin cgroup-lite libcgroup1
+apt-get install -y tar git curl nano wget dialog net-tools build-essential python2.7 python-pip freenect gstreamer-0.10 gstreamer0.10-plugins-base gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-good cgroup-bin cgroup-lite libcgroup1 daemon salt-minion
 pip install numpy
 EOF
 
@@ -25,6 +26,7 @@ fi
 
 # copy ros packages to workspace
 echo 'copy ros packages to workspace'
+echo '-----------------------'
 
 cd $BASEDIR
 
@@ -41,8 +43,16 @@ git clone https://furbaz:%40bitbucket2long@bitbucket.org/rapyutians/cloud_bridge
 cd /home/turtlebot/catkin_ws && catkin_make
 cd $BASEDIR
 
+## salt minion config
+echo 'copy salt minion config'
+echo '-----------------------'
+sudo bash << EOF
+yes | cp -rf salt/minion /etc/salt
+EOF
+
 # copy launch scripts
 echo 'copy ros packages to workspace'
+echo '-----------------------'
 cp launch_scripts/*.sh /home/turtlebot/
 
 
@@ -52,12 +62,16 @@ echo '-----------------------'
 cp pygst-kinect/ /home/turtlebot
 
 # copy rplidar daemon script
+echo 'create daemon for rplidar'
+echo '-----------------------'
 sudo bash << EOF
 cp udev/rplidar /etc/init.d
 chmod +x /etc/init.d/rplidar
 EOF
 
 # copy rplidar local rules
+echo 'copy udev rules'
+echo '-----------------------'
 sudo bash << EOF
 cp udev/10-local.rules /etc/udev/rules.d
 EOF
@@ -70,18 +84,27 @@ chmod +x /etc/udev/scripts/rplidar.sh
 EOF
 
 ## cgroup stuff
+echo 'copy cgroup conf'
+echo '-----------------------'
 sudo bash << EOF
 cp cgroups/cgconfig.conf /etc
 cp cgroups/cgrules.conf /etc
+yes | cp -rf cgroups/cgroup-lite.conf /etc/init
 
 cp cgroups/cgrulesengd.conf /etc/init
+EOF
+
+## rplidar conf
+echo 'copy rplidar conf'
+echo '-----------------------'
+sudo bash << EOF
+cp rplidar/rplidar.conf /etc/init
 EOF
 
 sudo bash << EOF
 service cgroup-lite start 
 EOF
 
-echo 'Installation complete'
-echo '-----------------------'
-
+echo 'Installation complete, please restart your system!'
+echo '--------------------------------------------------'
 
