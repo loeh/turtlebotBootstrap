60b685a7b3a435dcd0b5afc435179b69cc285730
Tobias <loeh@zhaw.ch>
2017-12-07 11:48:18 +0100

    removed repo which is outdated, no gets the current one via git updated the install script to work on a rpi with 16.04 and kinetic

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ USER='turtlebot'
 
 # install sources for salt-minion
 sudo bash << EOF
-wget -O - https://repo.saltstack.com/apt/ubuntu/14.04/amd64/latest/SALTSTACK-GPG-KEY.pub | apt-key add -
-echo 'deb http://repo.saltstack.com/apt/ubuntu/14.04/amd64/latest trusty main' > /etc/apt/sources.list.d/saltstack.list
+wget -O - https://repo.saltstack.com/apt/debian/8/armhf/2016.11/SALTSTACK-GPG-KEY.pub | apt-key add -
+echo 'deb http://repo.saltstack.com/apt/debian/8/armhf/2016.11 jessie main' > /etc/apt/sources.list.d/saltstack.list
 EOF
 
 # installing required packages
@@ -15,7 +15,7 @@ echo 'installing required packages'
 echo '-----------------------'
 sudo bash << EOF
 apt-get update
-apt-get install -y tar git curl nano wget dialog net-tools build-essential python2.7 python-pip freenect gstreamer-0.10 gstreamer0.10-plugins-base gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-good cgroup-bin cgroup-lite libcgroup1 daemon salt-minion ros-indigo-kobuki ros-indigo-kobuki-core python-gst0.10 doxygen graphviz mono-complete build-essential libusb-1.0-0-dev freeglut3-dev openjdk-7-jdk
+apt-get install -y tar git curl nano wget dialog net-tools build-essential python2.7 python-pip freenect gstreamer-0.10 gstreamer0.10-plugins-base gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-plugins-good cgroup-bin cgroup-lite libcgroup1 daemon salt-minion ros-kinetic-kobuki ros-kinetic-kobuki-core python-gst0.10 doxygen graphviz mono-complete build-essential libusb-1.0-0-dev freeglut3-dev openjdk-7-jdk
 pip install numpy
 EOF
 
@@ -24,8 +24,8 @@ echo 'installing docker'
 echo '-----------------------'
 sudo bash << EOF
 apt-get install apt-transport-https ca-certificates software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
 apt-get update
 apt-get install docker-ce
 EOF
@@ -34,14 +34,14 @@ EOF
 # create udev rule for kobuki
 echo 'create udev rule for kobuki'
 echo '-----------------------'
-source /opt/ros/indigo/setup.bash
+source /opt/ros/kinetic/setup.bash
 rosrun kobuki_ftdi create_udev_rules
 
 # create catkin_workspace
 echo 'create catkin_workspace'
 echo '-----------------------'
 if [ ! -d /home/$USER/catkin_ws ] ; then
-  source /opt/ros/indigo/setup.bash
+  source /opt/ros/kinetic/setup.bash
   mkdir -p /home/$USER/catkin_ws/src
   cd /home/$USER/catkin_ws/src && catkin_init_workspace
 else
@@ -55,8 +55,8 @@ echo '-----------------------'
 
 cd $BASEDIR
 
-if [ ! -d /home/$USER/catkin_ws/src/icclab_$USER ] ; then
-  cp -r icclab_turtlebot/ /home/$USER/catkin_ws/src/icclab_$USER
+if [ ! -d /home/$USER/catkin_ws/src/icclab_turtlebot ] ; then
+  git clone https://github.com/icclab/icclab_turtlebot.git -b kinetic  /home/$USER/catkin_ws/src/icclab_turtlebot
 else
   echo 'icclab package already exists'
   echo '-----------------------'
@@ -152,7 +152,7 @@ EOF
 echo 'copy DockDriveActionClient'
 echo '-----------------------'
 sudo bash << EOF
-yes | cp -rf docking/DockDriveActionClient.py /opt/ros/indigo/lib/kobuki_auto_docking
+yes | cp -rf docking/DockDriveActionClient.py /opt/ros/kinetic/lib/kobuki_auto_docking
 EOF
 
 # copy rplidar local rules
